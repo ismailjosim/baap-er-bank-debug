8ec3ad73685e5a9d879dd21c189d93b61e301452
ISMAIL JOSIM <ismailjosim>
2022-09-10 08:47:39 +0600

    deposit all error fixed!

--- js/utilities.js
+++ js/utilities.js
@@ -1,19 +1,28 @@
-function getInputFieldValueById(inputFieldId) {
-  const inputField = document.getElementById(inputFieldId);
-  const inputFieldValueString = inputField.value;
-  const inputFieldValue = parseFloat(inputFieldValueString);
-  return inputFieldValue;
-  inputField.value = "";
+// TODO: GET INPUT VALUE BY ID :
+const getInputFieldValueById = elementId => {
+  const inputEl = document.getElementById(elementId);
+  const inputValue = inputEl.value;
+  inputEl.value = '';
+  if (isNaN(inputValue) === true) {
+    alert("Enter a Number value!")
+    return 0;
+  } else {
+    const convertNumberValue = parseFloat(inputValue);
+    return convertNumberValue;
+  }
+
 }
 
-function getTextElementValueById(elementId) {
-  const textElement = document.getElementById(elementId);
-  const textElementValueString = textElement.innerText;
-  const textElementValue = textElementValueString;
-  textElementValue;
+// todo: get inner text by id :
+const getTextElementValueById = elementId => {
+  const textElement = document.getElementById(elementId).innerText;
+  const convertNumber = parseFloat(textElement);
+  return convertNumber;
 }
 
-function setTextElementValueById(elementId, newValue) {
+
+const setTextElementValueById = (elementId, newValue) => {
   const textElement = document.getElementById(elementId);
-  textElement.value = newValue;
+  textElement.innerText = newValue;
 }
+
