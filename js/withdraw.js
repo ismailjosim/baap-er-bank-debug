2e74366e3339df85b71450063c787f6701a9f666
ISMAIL JOSIM <ismailjosim>
2022-09-10 09:06:21 +0600

    withdrawal all error fix

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,20 +1,19 @@
-/*
-1. add withdraw button event handler
-2. get withdraw amount by using getInputFieldValueById function
-3. get previous withdraw amount by using getTextElementValueById function
-4. calculate new withdraw Total and set the value
-4-5: set new withdraw total by using setTextElementValueById function
-5. get previous balance total by using getTextElementValueById function
-6. calculate new balance total
-7. set balance total using setTextElementValueById
-*/
 
-document.getElementById("btn-withdraw").addEventListener("click", function () {
+document.getElementById("btn-withdraw").addEventListener("click", () => {
+  // fix: get value from withdrawal input field.
   const newWithdrawAmount = getInputFieldValueById("withdraw-field");
   const previousWithdrawTotal = getTextElementValueById("withdraw-total");
-  const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
-  setTextElementValueById(withdraw - total, newWithdrawTotal);
   const previousBalanceTotal = getTextElementValueById("balance-total");
-  const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
-  setTextElementValueById("balance-total", newBalanceTotal);
+
+  if (newWithdrawAmount > 0) {
+    if (newWithdrawAmount <= previousBalanceTotal) {
+      // FIX: SET INNER TEXT OF WITHDRAWAL AMOUNT :
+      const newWithdrawTotal = previousWithdrawTotal + newWithdrawAmount;
+      setTextElementValueById("withdraw-total", newWithdrawTotal);
+      const newBalanceTotal = previousBalanceTotal - newWithdrawAmount;
+      setTextElementValueById("balance-total", newBalanceTotal);
+    } else {
+      alert(`You Don't Have $${ newWithdrawAmount } In Your Account.`)
+    }
+  }
 });
